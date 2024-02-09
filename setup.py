26e5a201d5c34d1f433c9df5e237d4cfb84f7bdd
Reshma <reshmachikate5@gmail.com>
2024-02-09 16:12:11 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version="0.0.1",
+    description =' it a wineq package',
+    author='ReshmaChikate5',
+    packages= find_packages(),
+    license='MIT'
+)
